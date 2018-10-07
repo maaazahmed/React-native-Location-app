76922d7e6e5ea193cee8bc0b7a563440b977c5c7
maaazahmed <maazahmed2k16@gmail.com>
2018-10-07 18:44:08 +0500

    Updated

--- src/Comonents/ListAnimation/index.js
+++ src/Comonents/ListAnimation/index.js
@@ -81,3 +81,82 @@
 //     }
 // }
 
+
+
+
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Animated,
+    Dimensions,
+    Easing,
+    TouchableOpacity,
+    Button,
+} from 'react-native';
+import { Item } from 'native-base';
+
+
+
+
+export default class ListAnimation extends Component {
+    constructor() {
+        super()
+        this.index = 0
+        this.state = { valueArry: [] }
+        this.animatedValue = new Animated.Value(0)
+    }
+
+    addRow() {
+        this.animatedValue.setValue(0)
+        let newAdd = { index: this.index }
+        this.setState({ valueArry: [this.state.valueArry, newAdd] }, () => {
+            Animated.timing(this.animatedValue, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: true,
+            }).start(() => {
+                this.index = this.index + 1
+            })
+        })
+    }
+
+
+
+    render() {
+        const animatedPower = this.animatedValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: [-70, 0]
+        })
+
+
+        let rows = this.state.valueArry.map((val, index)=>{
+            
+        })
+
+
+        return (
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} >
+
+                <TouchableOpacity onPress={() => this.addRow()} activeOpacity={0.6} style={{
+                    height: 60,
+                    width: 60,
+                    backgroundColor: "green",
+                    position: "absolute",
+                    bottom: 30, right: 30,
+                    elevation: 10,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: 100
+                }} >
+                    <Text style={{ fontSize: 30, color: "#fff" }} >+</Text>
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+}
+
