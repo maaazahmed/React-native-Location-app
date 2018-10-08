56e2bcac9b3dace378c4b4bc9093560317193ddd
maaazahmed <maazahmed2k16@gmail.com>
2018-10-09 02:50:30 +0500

    Updated

--- src/Comonents/MaterialAnimatedCard/index.js
+++ src/Comonents/MaterialAnimatedCard/index.js
@@ -19,36 +19,64 @@ export default class MaiterailCard extends Component {
     constructor() {
         super()
         this.state = {
-            modalHeight: new Animated.Value(0)
+            modalHeight: new Animated.Value(0),
+            buttonBottomPosition: new Animated.Value(0)
         }
     }
 
+    onpenModal() {
+        Animated.parallel([
+            Animated.timing(this.state.modalHeight, {
+                toValue: 1,
+                duration: 500,
+                easing: Easing.elastic()
+            }),
+            Animated.timing(this.state.buttonBottomPosition, {
+                toValue: 1,
+                duration: 500,
+                easing: Easing.ease
+            })
+        ]).start()
+    }
+
     render() {
+        const height = this.state.modalHeight.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ["0%", "50%", "80%"],
+        })
+        const buttonBottom = this.state.buttonBottomPosition.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ["5%", "30%", "77%"],
+        })
+        const buttonLeft= this.state.buttonBottomPosition.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: ["5%", "20%", "42%"],
+        })
+
+
         return (
-            <View style={{ flex: 1, justifyContent: "center", backgroundColor: "#007867" }} >
+            <View style={{ flex: 1, justifyContent: "center", backgroundColor: "#11fbd8" }} >
                 <View style={{ flex: 1, justifyContent: "flex-end", elevation: 20 }}>
-                    <View style={{ backgroundColor: "#fff", height: "80%" }} >
-                    
-                    </View>
+                    <Animated.View style={{ backgroundColor: "#fff", height }} ></Animated.View>
                 </View>
-
-
-                <TouchableOpacity activeOpacity={.7} style={{
+                <Animated.View style={{
                     height: 60,
                     width: 60,
                     borderRadius: 100,
                     position: "absolute",
-                    bottom: 20, right: 20,
+                    bottom: buttonBottom,
+                    right: buttonLeft,
                     elevation: 20,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    backgroundColor: "#007867"
+                    backgroundColor: "#fff"
                 }} >
-                    <Text style={{ color: "#fff", fontSize: 30 }} >
-                        +
+                    <TouchableOpacity onPress={() => this.onpenModal()} activeOpacity={.7} style={{
+                        flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#8911fb", borderRadius: 100,
+                    }} >
+                        <Text style={{ color: "#fff", fontSize: 30 }} >
+                            +
                     </Text>
-                </TouchableOpacity>
-
+                    </TouchableOpacity>
+                </Animated.View>
 
             </View>
         )
