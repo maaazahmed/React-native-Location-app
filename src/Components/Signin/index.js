2c3e3ab3c533d54c591198d8469898a1e14f3825
maaazahmed <maazahmed2k16@gmail.com>
2018-10-11 20:38:51 +0500

    Updated

--- src/Components/Signin/index.js
+++ src/Components/Signin/index.js
@@ -7,21 +7,23 @@ import {
     Text,
     TextInput,
     TouchableOpacity,
-    Animated
-
+    Animated,
+    KeyboardAvoidingView,
+    Dimensions
 } from 'react-native';
 import Icons from "react-native-vector-icons/FontAwesome"
 import { Container, Header, Content, Form, Item, Input, Label, Icon } from 'native-base';
 import { TextField } from 'react-native-material-textfield';
 
 
-
+const { width } = Dimensions.get("window")
 export default class SignIn extends Component {
     constructor() {
         super()
         this.state = {
             buttonSignAnimmation: new Animated.Value(0),
-            buttonTextSignAnimmation: new Animated.Value(0)
+            buttonTextSignAnimmation: new Animated.Value(0),
+            signLoader: false
         }
 
     }
@@ -41,67 +43,71 @@ export default class SignIn extends Component {
         const { buttonSignAnimmation } = this.state
         const sighButtonWidth = buttonSignAnimmation.interpolate({
             inputRange: [0, 1],
-            outputRange:["100%", "12%"]
+            outputRange: ["100%", "12%"]
         })
 
 
 
         return (
-            <View style={styles.container}>
-                <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }} >
-                    {/* <Icons name="street-view" size={100} color="#fff" /> */}
-                    <Text style={{ color: "#fff", fontSize: 18, marginTop: 0 }} >F Location</Text>
+            <View style={styles.backgroundContainer} >
+                <View style={{ alignItems: "center", marginTop: 10 }} >
+                    <Icons name="street-view" size={100} color="#fff" />
+                    <Text style={{ color: "#fff", fontSize: 20 }} >F Location</Text>
                 </View>
-                <View style={{ flex: 2, alignItems: "center" }} >
-                    <View style={{ width: "90%", margin: 10, flex: 1, }} >
-                        <View style={{ flex: 1 }} >
-                            <TextField
-                                value=""
-                                tintColor="#fff"
-                                baseColor="#fff"
-                                labelTextStyle={{ color: "#fff" }}
-                                label='Email'
-                                textColor="#fff"
-                                fontSize={18}
-                                inputContainerPadding={15}
-                                labelFontSize={15}
-                                labelPadding={10}
-                            />
-                        </View>
-                        <View style={{ flex: 1 }}>
-                            <TextField
-                                value=""
-                                tintColor="#fff"
-                                baseColor="#fff"
-                                labelTextStyle={{ color: "#fff" }}
-                                label='Password'
-                                textColor="#fff"
-                                fontSize={18}
-                                inputContainerPadding={15}
-                                labelFontSize={15}
-                                labelPadding={10} />
-                        </View>
-                    </View>
-                    <View style={{ flex: 1, width: "90%" }} >
+                <View style={{ height: 100, width: width, alignItems: "center" }} >
+                    <TextField
+                        value=""
+                        inputContainerStyle={{ width: width - 55 }}
+                        tintColor="#fff"
+                        baseColor="#fff"
+                        labelTextStyle={{ color: "#fff" }}
+                        label='Email'
+                        textColor="#fff"
+                        fontSize={18}
+                        inputContainerPadding={15}
+                        labelFontSize={15}
+                        labelPadding={10}
 
-                        <View style={{ marginTop: 20, justifyContent:"center", alignItems:"center" }} >
-                            <Animated.View style={{ height: 50, width: sighButtonWidth, borderColor: "#fff", borderWidth: 1, borderRadius: 100, justifyContent: "center", alignItems: "center" }} >
-                                <TouchableOpacity
-                                    style={{ backgroundColor: "#312e3f", width: "100%", height: "100%", borderRadius: 100, justifyContent: "center", alignItems: "center" }}
-                                    onPress={() => { this.SignInHeandler() }}  >
-                                    {/* <Text style={{ color: "#fff", fontSize: 20 }}  >SIGN IN</Text> */}
-                                </TouchableOpacity>
-                            </Animated.View>
-                        </View>
-
-                        <View style={{ flex: 1, justifyContent: "flex-end", padding: 15, }} >
-                            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "flex-end", }} >
-                                <Text style={{ color: "#f2f2f2", fontSize: 15 }} >don't have an account yet, </Text>
-                                <TouchableOpacity>
-                                    <Text style={{ color: "#fff", fontSize: 18 }} > Sign Up </Text>
-                                </TouchableOpacity>
-                            </View>
-                        </View>
+                    />
+                </View>
+                <View style={{ height: 100, width: width, alignItems: "center" }} >
+                    <TextField
+                        value=""
+                        inputContainerStyle={{ width: width - 55 }}
+                        tintColor="#fff"
+                        baseColor="#fff"
+                        labelTextStyle={{ color: "#fff" }}
+                        label='Password'
+                        textColor="#fff"
+                        fontSize={18}
+                        inputContainerPadding={15}
+                        labelFontSize={15}
+                        labelPadding={10}
+                        secureTextEntry={true}
+                    />
+                    <View style={{ width: width - 55, marginTop: 20, alignItems: "center", justifyContent: "center" }} >
+                        <Animated.View style={{
+                            height: 50,
+                            width: sighButtonWidth,
+                            borderColor: "#fff",
+                            borderWidth: 1,
+                            borderRadius: 100,
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }} >
+                            <TouchableOpacity
+                                style={{
+                                    backgroundColor: "#312e3f",
+                                    width: "100%",
+                                    height: "100%",
+                                    borderRadius: 100,
+                                    justifyContent: "center",
+                                    alignItems: "center"
+                                }}
+                                onPress={() => { this.SignInHeandler() }}  >
+                                <Text style={{ color: "#fff", fontSize: 20 }}  >SIGN IN</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
                     </View>
                 </View>
             </View>
@@ -110,10 +116,14 @@ export default class SignIn extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    backgroundContainer: {
         flex: 1,
-        justifyContent: "flex-end",
+        justifyContent: "center",
+        alignItems: "center",
+        height: null,
+        width: null,
         backgroundColor: "#373447"
+        // backgroundColor:"rgba(55,52, 71, .8)"
     }
 });
 
