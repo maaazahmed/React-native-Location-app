fa28e162ff240919ac520f6b2ab188933bfa0664
maaazahmed <maazahmed2k16@gmail.com>
2018-10-12 23:21:10 +0500

    Updaated

--- src/index.js
+++ src/index.js
@@ -1,71 +1,135 @@
-// import React, { Component } from 'react';
-// import {
-//     View,
-// } from 'react-native';
-// import BottomNavigation, {
-//     FullTab
-// } from 'react-native-material-bottom-navigation'
+import React, { Component } from 'react';
+import {
+    View,
+} from 'react-native';
+import BottomNavigation, {
+    FullTab,
+    
+} from 'react-native-material-bottom-navigation'
+import Icon from "react-native-vector-icons/MaterialIcons"
+
+
+
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = { activeTab: 0 }
+
+    }
+
+
+    tabs = [
+        {
+            key: 'games',
+            icon: 'gamepad-variant',
+            label: 'Games',
+            barColor: '#388E3C',
+            pressColor: 'rgba(255, 255, 255, 0.16)'
+        },
+        {
+            key: 'movies-tv',
+            icon: 'movie',
+            label: 'Movies & TV',
+            barColor: '#B71C1C',
+            pressColor: 'rgba(255, 255, 255, 0.16)'
+        },
+        {
+            key: 'music',
+            icon: 'music-note',
+            label: 'Music',
+            barColor: '#E64A19',
+            pressColor: '#312e3f'
+        }
+    ]
+
+    renderIcon = icon => ({ isActive }) => (
+        <Icon size={24} color="white" name={icon} />
+    )
+
+    renderTab = ({ tab, isActive }) => {
+        return (
+            <View>
+                <FullTab
+                    isActive={isActive}
+                    key={tab.key}
+                    label={tab.label}
+                    renderIcon={this.renderIcon(tab.icon)}
+                />
+            </View>
+
+        )
+    }
+
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+
+                </View>
+                <BottomNavigation
+                    acactiveTab={this.state.activeTab}
+                    onTabPress={newTab => this.setState({ activeTab: newTab.key })}
+                    renderTab={this.renderTab}
+                    tabs={this.tabs}
+                >
+                </BottomNavigation>
+            </View>
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+// import * as React from 'react';
+// import { BottomNavigation, Text } from 'react-native-paper';
 // import Icon from "react-native-vector-icons/MaterialIcons"
+// import {Signin, SignUp, } from "./Components/index"
 
 
+// const MusicRoute = () => <Text>Music</Text>;
 
+// const AlbumsRoute = () => <Text>Albums</Text>;
 
-// export default class App extends React.Component {
-//     tabs = [
-//         {
-//             key: 'games',
-//             icon: 'gamepad-variant',
-//             label: 'Games',
-//             barColor: '#388E3C',
-//             pressColor: 'rgba(255, 255, 255, 0.16)'
-//         },
-//         {
-//             key: 'movies-tv',
-//             icon: 'movie',
-//             label: 'Movies & TV',
-//             barColor: '#B71C1C',
-//             pressColor: 'rgba(255, 255, 255, 0.16)'
-//         },
-//         {
-//             key: 'music',
-//             icon: 'music-note',
-//             label: 'Music',
-//             barColor: '#E64A19',
-//             pressColor: '#312e3f'
-//         }
-//     ]
+// const RecentsRoute = () => <Text>Recents</Text>;
 
-//     renderIcon = icon => ({ isActive }) => (
-//         <Icon size={24} color="white" name={icon} />
-//     )
+// // const a = () => <Text>Recents</Text>;
 
-//     renderTab = ({ tab, isActive }) => {
-//         console.log(tab, isActive)
-//         return (
-//             <FullTab
-//                 isActive={isActive}
-//                 key={tab.key}
-//                 label={tab.label}
-//                 renderIcon={this.renderIcon(tab.icon)}
-//             />
+// export default class MyComponent extends React.Component {
+//     state = {
+//         index: 0,
+//         routes: [
+//             { key: 'music', title: 'Friends', icon: 'place', color: '#312e3f' },
+//             { key: 'albums', title: 'Albums', icon: 'arrow-down', color: '#312e3f' },
+//             { key: 'recents', title: 'Recents', icon: 'colorize', color: '#312e3f' },
+//             { key: 'search', title: 'Search', icon: 'search', color: '#312e3f' },
+//         ],
+//     };
 
-//         )
-//     }
+//     _handleIndexChange = index => this.setState({ index });
+//     _renderScene = BottomNavigation.SceneMap({
+//         music: Signin,
+//         albums: SignUp,
+//         recents: RecentsRoute,
+//         search: RecentsRoute,
+//     });
+
 
 //     render() {
 //         return (
-//             <View style={{ flex: 1 }}>
-//                 <View style={{ flex: 1 }}>
-//                     {/* Your screen contents depending on current tab. */}
-
-//                 </View>
-//                 <BottomNavigation
-//                     onTabPress={newTab => this.setState({ activeTab: newTab.key })}
-//                     renderTab={this.renderTab}
-//                     tabs={this.tabs}
-//                 />
-//             </View>
-//         )
+//             <BottomNavigation
+//                 navigationState={this.state}
+//                 onIndexChange={this._handleIndexChange}
+//                 renderScene={this._renderScene}
+//             />
+//         );
 //     }
 // }
 
@@ -76,52 +140,70 @@
 
 
 
+// import React, { Component } from 'react'
+// import { View, StyleSheet } from 'react-native'
+// import BottomNavigation, { Tab } from 'react-native-material-bottom-navigation'
+// import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import * as React from 'react';
-import { BottomNavigation, Text } from 'react-native-paper';
-import Icon from "react-native-vector-icons/MaterialIcons"
+/**
+ * In this Example, the active Tab will be stored in the state.
+ */
 
+// export default class StatefulBottomNavigation extends Component {
+//   constructor(props) {
+//     super(props)
 
-const MusicRoute = () => <Text>Music</Text>;
+//     this.state = { activeTab: 0 }
+//     this.handleTabChange = this.handleTabChange.bind(this)
+//   }
 
-const AlbumsRoute = () => <Text>Albums</Text>;
+//   handleTabChange(newTabIndex, oldTabIndex) {
+//     this.setState({ activeTab: newTabIndex })
+//   }
 
-const RecentsRoute = () => <Text>Recents</Text>;
+//   render() {
+//     return (
+//       <View style={{ flex: 1 }}>
+//         <BottomNavigation
+//           activeTab={this.state.activeTab}
+//           labelColor="white"
+//           rippleColor="white"
+//           style={styles.bottomNavigation}
+//           onTabChange={this.handleTabChange}
 
-// const a = () => <Text>Recents</Text>;
+//         >
+//           <Tab
+//             barBackgroundColor="#37474F"
+//             label="Movies & TV"
+//             icon={<Icon size={24} color="white" name="ondemand-video" />}
+//           />
+//           <Tab
+//             barBackgroundColor="#00796B"
+//             label="Music"
+//             icon={<Icon size={24} color="white" name="music-note" />}
+//           />
+//           <Tab
+//             barBackgroundColor="#5D4037"
+//             label="Books"
+//             icon={<Icon size={24} color="white" name="book" />}
+//           />
+//           <Tab
+//             barBackgroundColor="#3E2723"
+//             label="Newsstand"
+//             icon={<Icon size={24} color="white" name="newspaper" />}
+//           />
+//         </BottomNavigation>
+//       </View>
+//     )
+//   }
+// }
 
-export default class MyComponent extends React.Component {
-    state = {
-        index: 0,
-        routes: [
-            { key: 'music', title: 'Friends', icon: 'place', color: '#312e3f' },
-            
-            { key: 'albums', title: 'Albums', icon: 'arrow-down', color: '#312e3f' },
-            { key: 'recents', title: 'Recents', icon: 'colorize', color: '#312e3f' },
-            { key: 'search', title: 'Search', icon: 'search', color: '#312e3f' },
-        ],
-    };
-
-    _handleIndexChange = index => this.setState({ index });
-    _renderScene = BottomNavigation.SceneMap({
-        music: MusicRoute,
-        albums: AlbumsRoute,
-        recents: RecentsRoute,
-        search: RecentsRoute,
-    });
-
-
-    render() {
-        return (
-            <BottomNavigation
-                navigationState={this.state}
-                onIndexChange={this._handleIndexChange}
-                renderScene={this._renderScene}
-            />
-        );
-    }
-}
-
-
-
-
+// const styles = StyleSheet.create({
+//   bottomNavigation: {
+//     position: 'absolute',
+//     left: 0,
+//     right: 0,
+//     bottom: 0,
+//     height: 56
+//   }
+// })
