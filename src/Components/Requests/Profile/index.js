610247cd9e897fe37c0dd33a63f0b307e277309f
maaazahmed <maazahmed2k16@gmail.com>
2018-10-20 17:05:11 +0500

    Updated

--- src/Components/Requests/Profile/index.js
+++ src/Components/Requests/Profile/index.js
@@ -16,7 +16,9 @@ export default class Profile extends Component {
                 <View style={{ backgroundColor: "#373447", width: width, height: 110, borderTopColor: "#312e3f", borderTopWidth: 5, justifyContent: "center", alignItems: "center", padding:10 }} >
                     <View style={{ flex: 1, backgroundColor: "green", flexDirection: "row", }} >
                         <View style={{ width: "20%", height: "100%", backgroundColor: "blue" }}>
-                          <Image>                                                         
+                          <Image 
+                          style={{height:%}}
+                          source={{uri:"https://avatars2.githubusercontent.com/u/31310451?s=88&v=4"}} />                                                         
                         </View>
                         <View style={{ width: "80%", height: "100%", backgroundColor: "yellow" }}>
                         </View>
