f887d9289493b3b23dd99eb3d1e80dbf5e3a6a67
maaazahmed <maazahmed2k16@gmail.com>
2018-10-20 21:35:49 +0500

    Updated

--- src/Components/Requests/Profile/index.js
+++ src/Components/Requests/Profile/index.js
@@ -7,7 +7,8 @@ import {
     TouchableOpacity,
     ScrollView,
     Animated,
-    Easing
+    Easing,
+    StyleSheet
 } from "react-native"
 import { Icon } from "native-base"
 import Icons from "react-native-vector-icons/FontAwesome"
@@ -19,64 +20,53 @@ export default class Profile extends Component {
     constructor() {
         super()
         this.ProfileDataAnim = new Animated.Value(0),
-        this.listOpacity = new Animated.Value(0)
+            this.listOpacity = new Animated.Value(0)
     }
 
 
-    componentDidMount(){
+    componentDidMount() {
         Animated.timing(this.listOpacity, {
             toValue: 1,
             duration: 500,
         }).start()
-        Animated.timing(this.ProfileDataAnim,{
-            toValue:1,
-            duration:500,
-            easing:Easing.elastic()
+        Animated.timing(this.ProfileDataAnim, {
+            toValue: 1,
+            duration: 500,
+            easing: Easing.elastic()
         }).start()
     }
-
-
-
-
-
-
 
     render() {
         let ProfileDataAnim = this.ProfileDataAnim.interpolate({
             inputRange: [0, 0.5, 1],
             outputRange: [10, 5, 0]
         })
-        
+
         let listOpacity = this.listOpacity.interpolate({
             inputRange: [0, 0.5, 1],
             outputRange: [0, 0.5, 1]
         })
 
         return (
-            <View style={{ flex: 1,}} >
-                <View>
+            <View style={{ flex: 1 }} >
+                <View style={{ marginTop: 15 }} >
                     <ScrollView style={{}} >
-                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 8, borderTopColor: "#312e3f", borderTopWidth: 5, justifyContent: "center", alignItems: "center", marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={[styles.profilePicCard, { marginTop: ProfileDataAnim, opacity: listOpacity }]} >
                             <View style={{ flex: 1, flexDirection: "row", }} >
-                                <View style={{ width: "20%", height: "100%", justifyContent: "center", alignItems: "center" }}>
+                                <View style={styles.profilePicCardContain}>
                                     <Image
-                                        style={{
-                                            height: 80,
-                                            width: 80,
-                                            borderRadius: width / 2
-                                        }}
+                                        style={styles.profilePic}
                                         source={{ uri: "https://avatars2.githubusercontent.com/u/31310451?s=88&v=4" }} />
                                 </View>
-                                <View style={{ width: "80%", height: "100%", justifyContent: "center", alignItems: "flex-end" }}>
-                                    <TouchableOpacity activeOpacity={0.5} style={{ height: 40, width: 40, borderRadius: width / 2, justifyContent: "center", alignItems: "center", padding: 5,  }} >
+                                <View style={styles.cameraContainer}>
+                                    <TouchableOpacity activeOpacity={0.5} style={{ height: 40, width: 40, borderRadius: width / 2, justifyContent: "center", alignItems: "center", padding: 5, }} >
                                         <Icon name="camera" style={{ fontSize: 27, color: "#ff2a68" }} />
                                     </TouchableOpacity>
                                 </View>
                             </View>
                         </Animated.View>
-
                         {/* 7777777777777777777777777777777 */}
-                        <Animated.View style={{ backgroundColor: "#373447",width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity: listOpacity }} >
                             <View style={{ justifyContent: "center", flex: 1 }} >
                                 <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
                                     <View style={{ flex: 1, padding: 5 }} >
@@ -91,7 +81,7 @@ export default class Profile extends Component {
                             </View>
                         </Animated.View>
                         {/* 7777777777777777777777777777777 */}
-                        <Animated.View style={{ backgroundColor: "#373447",width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity: listOpacity }} >
                             <View style={{ justifyContent: "center", flex: 1 }} >
                                 <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
                                     <View style={{ flex: 5, padding: 5 }} >
@@ -106,7 +96,7 @@ export default class Profile extends Component {
                             </View>
                         </Animated.View>
                         {/* 7777777777777777777777777777777 */}
-                        <Animated.View style={{ backgroundColor: "#373447",width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity: listOpacity }} >
                             <View style={{ justifyContent: "center", flex: 1 }} >
                                 <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
                                     <View style={{ flex: 5, padding: 5 }} >
@@ -121,7 +111,7 @@ export default class Profile extends Component {
                             </View>
                         </Animated.View>
                         {/* 7777777777777777777777777777777 */}
-                        <Animated.View style={{ backgroundColor: "#373447",width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity: listOpacity }} >
                             <View style={{ justifyContent: "center", flex: 1 }} >
                                 <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
                                     <View style={{ flex: 5, padding: 5 }} >
@@ -136,7 +126,7 @@ export default class Profile extends Component {
                             </View>
                         </Animated.View>
                         {/* 7777777777777777777777777777777 */}
-                        <Animated.View style={{ backgroundColor: "#373447",width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity:listOpacity  }} >
+                        <Animated.View style={{ backgroundColor: "#373447", width: "100%", height: height / 13, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10, marginTop: ProfileDataAnim, opacity: listOpacity }} >
                             <View style={{ justifyContent: "center", flex: 1 }} >
                                 <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
                                     <View style={{ flex: 5, padding: 5 }} >
@@ -156,3 +146,34 @@ export default class Profile extends Component {
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    profilePicCard: {
+        padding: 10,
+        backgroundColor: "#373447",
+        width: "100%",
+        height: height / 8,
+        borderTopColor: "#312e3f",
+        borderTopWidth: 5,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    profilePicCardContain: {
+        width: "20%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    profilePic: {
+        height: 80,
+        width: 80,
+        borderRadius: width / 2
+    },
+    cameraContainer: {
+        width: "80%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "flex-end"
+    }
+})
