e42b85a59aaf724b64bfde844f332cf544b4b21d
maaazahmed <maazahmed2k16@gmail.com>
2018-10-20 19:07:58 +0500

    Updated

--- src/Components/Requests/Profile/index.js
+++ src/Components/Requests/Profile/index.js
@@ -8,7 +8,7 @@ import {
 } from "react-native"
 import { Icon } from "native-base"
 import Icons from "react-native-vector-icons/MaterialCommunityIcons"
- 
+
 
 const { width, height } = Dimensions.get("window")
 export default class Profile extends Component {
@@ -35,13 +35,14 @@ export default class Profile extends Component {
                     </View>
                 </View>
 
-                <View style={{ backgroundColor: "#373447", width: width, height: 60, borderTopColor: "#312e3f", borderTopWidth: 5, padding:10 }} >
+                {/* 7777777777777777777777777777777 */}
+                <View style={{ backgroundColor: "#373447", width: width, height: 60, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10 }} >
                     <View style={{ justifyContent: "center", flex: 1 }} >
                         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
-                            <View style={{ flex: 3, paddingLeft:10 }} >
-                                <Text style={{fontSize:17, color:"#fff", fontWeight:"300"}} >Maaz Ahmed</Text>
+                            <View style={{ flex: 1, padding: 5}} >
+                                <Text style={{ fontSize: 17, color: "#fff", fontWeight: "300" }} >Maaz Ahmed</Text>
                             </View>
-                            <View style={{ flex: 1, alignItems:"flex-end" }} >
+                            <View style={{ width: "10%", alignItems: "flex-end" }} >
                                 <TouchableOpacity activeOpacity={0.5} style={{ height: 40, width: 40, borderRadius: width / 2, justifyContent: "center", alignItems: "center", padding: 5 }} >
                                     <Icon name="create" style={{ fontSize: 27, color: "#ff2a68" }} />
                                 </TouchableOpacity>
@@ -49,13 +50,14 @@ export default class Profile extends Component {
                         </View>
                     </View>
                 </View>
-                <View style={{ backgroundColor: "#373447", width: width, height: 60, borderTopColor: "#312e3f", borderTopWidth: 5, padding:10 }} >
+                {/* 7777777777777777777777777777777 */}
+                <View style={{ backgroundColor: "#373447", width: width, height: 60, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10 }} >
                     <View style={{ justifyContent: "center", flex: 1 }} >
                         <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
-                            <View style={{ flex: 3, }} >
-                                <Text style={{fontSize:17, color:"#fff", fontWeight:"300"}} >maazahmed2k16@gmail.com</Text>
+                            <View style={{ flex: 5, padding: 5}} >
+                                <Text style={{ fontSize: 17, color: "#fff", fontWeight: "300" }} >maazahmed2k16@gmail.com</Text>
                             </View>
-                            <View style={{ flex: 1, alignItems:"flex-end" }} >
+                            <View style={{ width: "10%", alignItems: "flex-end" }} >
                                 <TouchableOpacity activeOpacity={0.5} style={{ height: 40, width: 40, borderRadius: width / 2, justifyContent: "center", alignItems: "center", padding: 5 }} >
                                     <Icon name="create" style={{ fontSize: 27, color: "#ff2a68" }} />
                                 </TouchableOpacity>
@@ -63,6 +65,22 @@ export default class Profile extends Component {
                         </View>
                     </View>
                 </View>
+                {/* 7777777777777777777777777777777 */}
+                <View style={{ backgroundColor: "#373447", width: width, height: 60, borderTopColor: "#312e3f", borderTopWidth: 5, padding: 10 }} >
+                    <View style={{ justifyContent: "center", flex: 1 }} >
+                        <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center" }} >
+                            <View style={{ flex: 5, padding: 5}} >
+                                <Text style={{ fontSize: 17, color: "#fff", fontWeight: "300" }} >Location</Text>
+                            </View>
+                            <View style={{ width: "10%", alignItems: "flex-end" }} >
+                                <TouchableOpacity activeOpacity={0.5} style={{ height: 40, width: 40, borderRadius: width / 2, justifyContent: "center", alignItems: "center", padding: 5 }} >
+                                    <Icons2 name="place" style={{ fontSize: 27, color: "#ff2a68" }} />
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+
             </View>
         )
     }
