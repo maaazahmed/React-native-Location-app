b05f3079b615c0e38b174374370b6c96effafe50
maaazahmed <maazahmed2k16@gmail.com>
2018-10-20 16:50:39 +0500

    Updated

--- src/Components/Requests/Profile/index.js
+++ src/Components/Requests/Profile/index.js
@@ -11,10 +11,13 @@ const { width, height } = Dimensions.get("window")
 export default class Profile extends Component {
     render() {
         return (
-            <View style={{flex:1}} >
-                  <View style={{backgroundColor:"green", }} >
+            <View style={{ flex: 1 }} >
+                <View style={{ backgroundColor: "#373447", width: width, height: 100, borderTopColor: "#312e3f", borderTopWidth: 5 }} >
 
-                  </View>
+                </View>
+                <View style={{ backgroundColor: "#373447", width: width, height: 100, borderTopColor: "#312e3f", borderTopWidth: 5 }} >
+
+                </View>
             </View>
         )
     }
