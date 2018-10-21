11aabd62abd57202a4fa32bb1e19152ed1ffee30
maaazahmed <maazahmed2k16@gmail.com>
2018-10-21 22:05:36 +0500

    Updated

--- src/Components/Requests/index.js
+++ src/Components/Requests/index.js
@@ -225,12 +225,7 @@ const styles = StyleSheet.create({
     //         flexDirection: "row",
     //     },
 
-    //     searcBarContainerr: {
-    //         flex: 1,
-    //         flexDirection: "row",
-    //         justifyContent: "space-between",
-    //         alignItems: "center",
-   
+  
 
   
 
