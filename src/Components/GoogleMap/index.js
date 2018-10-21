7866f9ac0afd9c426b87090db6d0dda52f1b469a
maaazahmed <maazahmed2k16@gmail.com>
2018-10-21 22:28:38 +0500

    Updated

--- src/Components/GoogleMap/index.js
+++ src/Components/GoogleMap/index.js
@@ -311,54 +311,6 @@ const styles = StyleSheet.create({
 
 
 
-
-// import React, { Component } from "react";
-// import {
-//   AppRegistry,
-//   StyleSheet,
-//   Text,
-//   View,
-//   ScrollView,
-//   Animated,
-//   Image,
-//   Dimensions,
-
-// } from "react-native";
-
-// import MapView,{UrlTile} from "react-native-maps";
-
-// const Images = [
-//   { uri: "https://i.imgur.com/sNam9iJ.jpg" },
-//   { uri: "https://i.imgur.com/N7rlQYt.jpg" },
-//   { uri: "https://i.imgur.com/UDrH0wm.jpg" },
-//   { uri: "https://i.imgur.com/Ka8kNST.jpg" }
-// ]
-
-// const { width, height } = Dimensions.get("window");
-
-// const CARD_HEIGHT = height / 4;
-// const CARD_WIDTH = CARD_HEIGHT - 50;
-
-// export default class screens extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       markers: [
-//         {
-//           coordinate: {
-//             latitude: 24.8937322,
-//             longitude: 67.0287092,
-//           },
-//           title: "Best Place",
-//           description: "This is the best place in Portland",
-//           image: Images[0],
-//         },
-//         {
-//           coordinate: {
-//             latitude: 24.8937311,
-//             longitude: 67.0287022,
-
-//           },
 //           title: "Second Best Place",
 //           description: "This is the second best place in Portland",
 //           image: Images[1],
