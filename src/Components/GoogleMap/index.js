b64a4a1c3cfd316d2d563ae196c93817e0ef604d
maaazahmed <maazahmed2k16@gmail.com>
2018-11-05 20:24:47 +0500

    Updated

--- src/Components/GoogleMap/index.js
+++ src/Components/GoogleMap/index.js
@@ -238,6 +238,152 @@
 
 
 
+// import React, { Component } from 'react';
+// import { AppRegistry, StyleSheet, View, Dimensions } from 'react-native';
+// import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+// import RetroMapStyles from './mapStyle.json';
+// let { width, height } = Dimensions.get('window');
+// const ASPECT_RATIO = width / height;
+// const LATITUDE = 0;
+// const LONGITUDE = 0;
+// const LATITUDE_DELTA = 0.0922;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// export default class Map extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       region: {
+//         latitude: LATITUDE,
+//         longitude: LONGITUDE,
+//         latitudeDelta: LATITUDE_DELTA,
+//         longitudeDelta: LONGITUDE_DELTA,
+//       }
+//     };
+//   }
+//   componentDidMount() {
+//     navigator.geolocation.getCurrentPosition(
+//       position => {
+//         this.setState({
+//           region: {
+//             latitude: position.coords.latitude,
+//             longitude: position.coords.longitude,
+//             latitudeDelta: LATITUDE_DELTA,
+//             longitudeDelta: LONGITUDE_DELTA,
+//           }
+//         });
+//       },
+//     (error) => console.log(error.message),
+//     { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+//     );
+//     this.watchID = navigator.geolocation.watchPosition(
+//       position => {
+//         this.setState({
+//           region: {
+//             latitude: position.coords.latitude,
+//             longitude: position.coords.longitude,
+//             latitudeDelta: LATITUDE_DELTA,
+//             longitudeDelta: LONGITUDE_DELTA,
+//           }
+//         });
+//       }
+//     );
+//   }
+//   componentWillUnmount() {
+//     navigator.geolocation.clearWatch(this.watchID);
+//   }
+//   render() {
+//     return (
+//       <MapView
+//         provider={ PROVIDER_GOOGLE }
+//         zoomLevel={100}
+//         loadingEnabled
+//         scrollEnabled
+//         showsUserLocation={true}
+//         zoomEnabled
+//         pitchEnabled
+//         rotateEnabled
+//         style={{height:"100%", width:"100%"} }
+//         showsUserLocation={ true }
+//         region={ this.state.region }
+//         onRegionChange={ region => this.setState({region}) }
+//         onRegionChangeComplete={ region => this.setState({region}) }
+//       >
+//         <MapView.Marker
+//           coordinate={ this.state.region }
+//         />
+//       </MapView>
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <View style={styles.container}></View>
+//         <MapView
+//           zoomLevel={100}
+//           loadingEnabled
+//           scrollEnabled
+//           zoomEnabled
+//           pitchEnabled
+//           rotateEnabled
+//           onRegionChange={(ev)=>console.log(ev)}
+//           ref={map => this.map = map}
+//           initialRegion={{
+//             latitude: this.state.latitude,
+//             longitude: this.state.longitude,
+//             latitudeDelta: this.state.latitudeDelta,
+//             longitudeDelta: this.state.longitudeDelta,
+//           }}
+//           style={styles.container}
+
+//         >
+//           {this.state.markers.map((marker, index) => {
+//             return (
+//               <MapView.Marker
+//                key={index}
+//                 title="Maaz Ahmed"
+//                 coordinate={marker}>
+//                 <View style={[styles.markerWrap]}>
+//       
+
+
+
+
+
+
+
+
+
+
+
 
 
 
