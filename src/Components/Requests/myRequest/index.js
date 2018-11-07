8f3b12dc82f1ab3a08b5e1a106173ae820caebd3
maaazahmed <maazahmed2k16@gmail.com>
2018-11-08 00:37:55 +0500

    Updatedd

--- src/Components/Requests/myRequest/index.js
+++ src/Components/Requests/myRequest/index.js
@@ -70,13 +70,7 @@ let arr = [
         lastSeen: "04:30 AM",
         pic: "https://tse3.mm.bing.net/th?id=OIP.rHVsvkMy5Z93RSgBSa2qBgHaE7&pid=15.1&P=0&w=230&h=153"
     },
-    {
-        username: "Aslam Khan",
-        email: "aslam@gmail.com",
-        lastSeen: "04:30 AM",
-        pic: "https://tse1.mm.bing.net/th?id=OIP.o5mjydXPukRieEiTAETvPQHaKK&pid=15.1&P=0&w=300&h=300"
-
-    },
+   
    
 ]
 
