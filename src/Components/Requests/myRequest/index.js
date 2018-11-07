1aecb6ebdf3c50f7f6e4e816edb425017dab2239
maaazahmed <maazahmed2k16@gmail.com>
2018-11-08 00:38:12 +0500

    Updatedd

--- src/Components/Requests/myRequest/index.js
+++ src/Components/Requests/myRequest/index.js
@@ -57,13 +57,7 @@ let arr = [
         pic: "https://tse1.mm.bing.net/th?id=OIP.G-aZmAKu77bzDA8JuXBS3AAAAA&pid=15.1&P=0&w=300&h=300"
 
     },
-    {
-        username: "Haris Ahmed",
-        email: "haris@gmail.com",
-        lastSeen: "04:30 AM",
-        pic: "https://tse2.mm.bing.net/th?id=OIP.8UiX79bKZsDXbD-bIUz7AAHaJ4&pid=15.1&P=0&w=300&h=300"
-
-    },
+ 
   
    
    
