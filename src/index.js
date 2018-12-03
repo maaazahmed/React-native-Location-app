7feed14b7bc9a0777a8ae2c86a35bc2e1f9bc79f
maaazahmed <maazahmed2k16@gmail.com>
2018-12-03 10:20:18 +0500

    Updated

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ class Dashboard extends React.Component {
         let friendList = this.props.myFriendsList.friendList
         const currentUser = this.props.currentUser.currentUser;
         for (let i = 0; i < friendList.length; i++) {
-            const element = friendList[i];
+            const element = friendList[0];
             console.log(element)
             database.child(`friends/${element.nodeKey}/${currentUser.id}`).set(false)
         }
